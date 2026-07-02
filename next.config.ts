f9d530710d902d36253f7b3a1e71c6c96784a9b6
arvind2602 <guptaarvind2602@gmail.com>
2026-07-02 10:48:03 +0530

    refactor: remove custom webpack configuration for WebAssembly and layers from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.experiments = { ...config.experiments, asyncWebAssembly: true, layers: true };
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
